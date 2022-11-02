e9fed46098805d9807a341ab65aa3762bfb9cf66
Karim ESSAKHI <karim.essakhi@epita.fr>
2022-11-02 20:29:16 +0100

    cleaned files

--- expense.py
+++ expense.py
@@ -20,31 +20,13 @@ expense_questions = [
 
 ]
 
-user_questions = [
-    {
-        "type":"input",
-        "name":"user",
-        "message":"New user - Name: ",
-    },
-]
-
-
 
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on expense_report.csv
     f = open('./expense_report.csv', 'a') # Open the file in write mode
-    f.write("\n") #else it appends directly to last line
     f.write(infos['amount'] + "," + infos['label'] + "," + infos['spender'])
+    f.write("\n") #else it appends directly to last line
     f.close()
     print("Expense Added !")
-    return True
-
-def new_user(*args):
-    infos = prompt(user_questions)
-    f = open('./users.csv', 'a') # Open the file in write mode
-    f.write("\n") #else it appends directly to last line
-    f.write(infos['user'])
-    f.close()
-    print("User Added !")
     return True
--- user.py
+++ user.py
@@ -1,8 +1,23 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"user",
+        "message":"New user - Name: ",
+    },
 ]
 
-def add_user():
-    # This function should create a new user, asking for its name
-    return
+def add_user(*args):
+    infos = prompt(user_questions)
+    if (infos['user'] == "" or infos['user'] == " "):
+        print("Mandatory user information : Name")
+        add_user(*args)
+        return False
+    f = open('./users.csv', 'a') # Open the file in write mode
+    f.write(infos['user'])
+    f.write("\n") #else it appends directly to last line
+    f.close()
+    print("User Added !")
+    return True
