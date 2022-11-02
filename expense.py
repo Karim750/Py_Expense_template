7c19154016823f0243f07c73796533079d740dd5
Karim ESSAKHI <karim.essakhi@epita.fr>
2022-11-02 20:49:42 +0100

    [DONE] Adding existing users

--- expense.py
+++ expense.py
@@ -12,21 +12,31 @@ expense_questions = [
         "name":"label",
         "message":"New Expense - Label: ",
     },
-    {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
-    },
 
 ]
-
 
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on expense_report.csv
     f = open('./expense_report.csv', 'a') # Open the file in write mode
-    f.write(infos['amount'] + "," + infos['label'] + "," + infos['spender'])
+    f.write(infos['amount'] + "," + infos['label'] + "," + get_users())
     f.write("\n") #else it appends directly to last line
     f.close()
     print("Expense Added !")
     return True
+
+#get users to the cli when adding a new expense
+def get_users():
+    with open('./users.csv', 'r') as f:
+        reader = csv.reader(f)
+        users = []
+        for row in reader:
+            users.append(row[0])
+    users_list = {
+        "type":"list",
+        "name":"users_list",
+        "message":"New Expense - Spender: ",
+        "choices": users
+    }
+    option = prompt(users_list)
+    return option['users_list']
